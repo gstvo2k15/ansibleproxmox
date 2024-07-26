7689f61a3df5431c742dad952b192a5a501133f1
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-07-26 23:38:10 +0200

    Added ansible-roles config and terraform-last1a

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,7 +13,7 @@ provider "proxmox" {
   pm_password     = var.proxmox_password  
   #pm_api_token_id     = "root@pam!asdasdas"
   #pm_api_token_secret = var.proxmox_token_secret
-  pm_tls_insecure    = false
+  pm_tls_insecure    = true
   pm_debug           = true
 }
 
