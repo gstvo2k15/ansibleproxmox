bdbf37e7c54cf8c4d0e70aa66fb4410228e2dbdd
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-07-26 23:05:32 +0200

    Added ansible-roles config and terraform-end7f

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "proxmox" {
 
 resource "proxmox_vm_qemu" "base" {
   name        = "ubuntu-base"
-  target_node = "pve"
+  target_node = "var.proxmox_node_ip"
   cores       = 2
   memory      = 4096
   disk {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,3 +11,10 @@ variable "proxmox_password" {
   description = "Proxmox password"
   type        = string
 }
+
+variable "proxmox_node_ip" {
+  description = "IP address of the Proxmox node"
+  type        = string
+  default     = "192.168.1.250"
+}
+
