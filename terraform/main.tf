78c73fc94b61dd5d47455dabddeb6ac055f01185
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-07-26 23:13:54 +0200

    Added ansible-roles config and terraform-end8c

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,32 @@ provider "proxmox" {
   pm_tls_insecure = true  # Deshabilita la verificación del certificado SSL
 }
 
+resource "proxmox_vm_qemu" "base" {
+  name        = "ubuntu-base"
+  target_node = var.proxmox_node_ip
+  vmid        = 700  # ID for the base VM
+  cores       = 2
+  memory      = 4096
+  disk {
+    size    = "30G"
+    storage = "local-lvm"
+    type    = "scsi"
+  }
+  network {
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+  iso = "local:iso/ubuntu-22.04.iso"
+  os_type = "cloud-init"
+}
+
+resource "null_resource" "convert_to_template" {
+  provisioner "local-exec" {
+    command = "pvesh set /nodes/${var.proxmox_node_ip}/qemu/${proxmox_vm_qemu.base.id}/template"
+  }
+  depends_on = [proxmox_vm_qemu.base]
+}
+
 resource "proxmox_vm_qemu" "vm" {
   count       = length(var.vm_list)
   name        = var.vm_list[count.index].name
@@ -34,10 +60,6 @@ resource "proxmox_vm_qemu" "vm" {
   os_type = "cloud-init"
   ciuser     = "ubuntu"
   cipassword = "ubuntu_password"
-  #sshkeys    = file("/root/.ssh/id_rsa.pub")
-  #provisioner "local-exec" {
-  #  command = "echo ${self.default_ipv4_address} >> ../ansible/inventory/hosts"
-  #}
 }
 
 output "vm_ips" {
