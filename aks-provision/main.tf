10f269daf98c0d0427f56b6f95eba33c070d9572
gstvo2k15 <48087822+gstvo2k15@users.noreply.github.com>
2024-08-17 20:03:17 +0200

    Added initial files

---
+++ aks-provision/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  # Se asume que estas variables de entorno están configuradas correctamente
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "tenant_id" {
+  type = string
+}
+
+module "aks_example_named_cluster" {
+  source  = "Azure/aks/azurerm//examples/named_cluster"
+  version = "6.2.0"
+}
